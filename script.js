b05c57cafbd99c15527b9871037c8c9abba23729
mekaala <mekaala@gmail.com>
2020-02-20 01:22:34 -0500

    created nav bar, quick links

--- script.js
+++ script.js
@@ -1,35 +1,30 @@
-const about = document.getElementById("modalAbout");
-
-// Get the button that opens the modal
-const buttons = document.getElementById("aboutMe");
-const span = document.getElementsByClassName("close")[0];
-span.onclick = function() {
-    about.style.display = "none";
-  }
-
-// When the user clicks on the button, open the modal
-buttons.onclick = function() {
-  about.style.display = "block";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == about) {
-    about.style.display = "none";
-  }
-}
-
 const body = document.querySelector('body');
 const nightMode = document.querySelector(".nMode");
 nightMode.onclick = function() {
-    body.classList.toggle('night-mode');    
+  body.classList.toggle('night-mode');    
     if (nightMode.innerHTML === "Night Mode") {
         nightMode.innerHTML = "Day Mode";
         nightMode.classList.remove('nMode');
         nightMode.classList.add('dMode');
-      } else {
-        nightMode.innerHTML = "Night Mode";
-        nightMode.classList.remove('dMode');
-        nightMode.classList.add('nMode');
-      }
+        navbar.classList.add('dayMode');
+    } else {
+      nightMode.innerHTML = "Night Mode";
+      nightMode.classList.remove('dMode');
+      nightMode.classList.add('nMode');
+      navbar.classList.remove('dayMode');
+    }
+}
+
+window.onscroll = function() {myFunction()};
+
+const navbar = document.getElementById("navbar");
+
+const sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
 }
